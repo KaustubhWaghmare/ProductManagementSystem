a80c2b5016653d77e784a4b9dd2d3f3f8daefcd9
KaustubhWaghmare <137987014+KaustubhWaghmare@users.noreply.github.com>
2023-06-28 12:45:08 +0530

    Add files via upload

---
+++ Product_Management_Backend/src/main/java/com/becoder/service/ProductServiceImpl.java
@@ -0,0 +1,51 @@
+package com.becoder.service;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.becoder.model.Product;
+import com.becoder.repository.ProductRepository;
+
+
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	@Autowired
+	private ProductRepository productRepo;
+
+	@Override
+	public Product saveProduct(Product product) {
+		// TODO Auto-generated method stub
+		return productRepo.save(product);
+	}
+
+	@Override
+	public List<Product> getAllProduct() {
+		// TODO Auto-generated method stub
+		return productRepo.findAll();
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return productRepo.findById(id).get();
+	}
+
+	@Override
+	public String deleteProduct(Integer id) {
+		// TODO Auto-generated method stub
+		Product product = productRepo.findById(id).get();
+		
+		if(product!= null)
+		{
+			productRepo.delete(product);
+			return "Product delete Successfully";
+		}
+		return "Something Went Wrong On Server";
+	}
+	
+
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+• Product Management System 
+
+This Mini Project includes basic CRUD operations involving front end in React.js, 
+middle tier in java and for database MySQL is used. it will store basic information of products like ID,Product Name,Description,Price 
+and status into Database via Front end and Middle tier along with this Update and Delete options are provided for clients convenience. 
+
+Platform : Java EE, React.Js, MySQL 
